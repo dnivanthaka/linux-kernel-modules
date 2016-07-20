6833548fb28ed11008ca11793df0cc76be994664
dnivanthaka <nivanthaka@gmail.com>
2016-07-20 23:25:33 +0530

    Updated the files with gpio output

---
+++ timers/timergpio.c
@@ -0,0 +1,58 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/timer.h>
+#include <linux/gpio.h>
+
+#define TIMER_INTERVAL 1000
+
+static struct timer_list my_timer;
+static unsigned int gpioLED = 17;
+static bool ledOn = 0;
+
+void my_timer_callback(unsigned long data){
+    int ret;
+    
+    ledOn = !ledOn;
+
+    gpio_set_value(gpioLED, ledOn);
+
+    printk(KERN_INFO "my_timer_callback called (%ld).\n", jiffies);
+    ret = mod_timer(&my_timer, jiffies + msecs_to_jiffies(TIMER_INTERVAL));
+
+    if(ret)
+        printk(KERN_INFO "Error in mod_timer\n");
+}
+
+int init_module(){
+    int ret;
+    printk(KERN_INFO "Timer module installing\n");
+
+    ledOn = true;
+    gpio_request(gpioLED, "sysfs");
+    gpio_direction_output(gpioLED, ledOn);
+    gpio_export(gpioLED, false);
+
+    setup_timer(&my_timer, my_timer_callback, 0);
+    printk(KERN_INFO "Starting timer to fire in %dms (%ld)\n", TIMER_INTERVAL, jiffies);
+    ret = mod_timer(&my_timer, jiffies + msecs_to_jiffies(TIMER_INTERVAL));
+
+    if(ret)
+        printk(KERN_INFO "Error in mod_timer\n");
+
+    return 0;
+}
+
+
+void cleanup_module(){
+    int ret;
+
+    ret = del_timer(&my_timer);
+
+    if(ret)
+        printk("The timer is still in use..\n");
+
+    printk(KERN_INFO "Timer module uninstalling\n");
+
+    return;
+}
+MODULE_LICENSE("GPL");
